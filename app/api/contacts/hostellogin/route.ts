667f5f6abdd67289c14c7f5b2013d67860670f06
gideondakore <armstrongspycon27@gmail.com>
2024-01-08 15:07:06 +0000

    Improved sign up page to avoid multiple account creations

--- app/api/contacts/hostellogin/route.ts
+++ app/api/contacts/hostellogin/route.ts
@@ -1,5 +1,4 @@
 import Contact from "@/app/models/contact";
-import checkPasswordValidity from "@/app/libs/checkPasswordValidity";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET (request: NextRequest, response: NextResponse) {
@@ -18,10 +17,10 @@ export async function GET (request: NextRequest, response: NextResponse) {
         ]
         });
 
-          if(user)
-           return NextResponse.json({user, message: "successfully", success: true}, {status: 200});
-          else
-           return NextResponse.json({user, message: "user not found", success: false}, {status: 201});
+      if(user)
+       return NextResponse.json({user, message: "successfully", success: true}, {status: 200});
+      else
+       return NextResponse.json({user, message: "user not found", success: false}, {status: 201});
         
   } catch (error) {
       return NextResponse.json({error: error, success: false}, {status: 500});
--- app/api/contacts/route.ts
+++ app/api/contacts/route.ts
@@ -4,17 +4,18 @@ import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest, response: NextResponse) {
-    
     try {
       const body = await request.json();
       const userEmailOrPhone = body.email ?? body.phone;
       const userPassword = body.password;
 
+      const keysArr = Object.keys(body)
+      const emailOrPhone = keysArr[1];
+
       const validEmailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
       const validPhoneRegex = /^(?:[+]?[2][3][3]|0)?[1-9]\d{8}$/;
       let validatedCredentials:string[] = [];
 
-      
       if(userPassword){
         validatedCredentials.push(...checkPasswordValidity(userPassword));
         }else{
@@ -31,11 +32,22 @@ export async function POST(request: NextRequest, response: NextResponse) {
       }
         
       if(Array.isArray(validatedCredentials) && (validatedCredentials.length === 0)){
+
+          const res = await fetch(`http://localhost:3000/api/contacts/?${emailOrPhone}=${userEmailOrPhone}`);
+          if(!res.ok){
+            return NextResponse.json({message: ["Ooops! somthing went wrong, try again later"], success: false}, {status: 500});
+          }
+
+          const {message, success} = await res.json();
+          if(!success){
+            return NextResponse.json({message: message, success: false}, {status: 201});
+          }
+
           await Contact.create(body);
-          return NextResponse.json({msg: ["Account created successfully"], success: true}, {status: 200});
-        }
+          return NextResponse.json({message: ["Account created successfully"], success: true}, {status: 200});
+      }
         console.log(validatedCredentials.length !== 0);
-        return NextResponse.json({msg: validatedCredentials, success: false}, {status: 500});
+        return NextResponse.json({message: validatedCredentials, success: false}, {status: 500});
 
     } catch (error) {
         if(error instanceof mongoose.Error.ValidationError){
@@ -43,12 +55,31 @@ export async function POST(request: NextRequest, response: NextResponse) {
             for(let e in error.errors){
                 errorList.push((error.errors[e].message));
             }
-            return NextResponse.json({msg: errorList});
+            return NextResponse.json({message: errorList});
         }else{
-            return NextResponse.json({msg: ["Unable to send message"]});
+            return NextResponse.json({message: ["Unable to send message"]});
         }
     }
 }
 
+export async function GET(request: NextRequest, response: NextResponse){
+    const {searchParams} = new URL(request.url);
+    const userEmailOrPhone = searchParams.get('email') ?? searchParams.get('phone');
+    
+    const keysIterator = searchParams.keys();
+    const firstKey = keysIterator.next().value;
+    
+    const user = await Contact.findOne({
+        $or: [
+            {email: userEmailOrPhone},
+            {phone: userEmailOrPhone},
+        ]
+    })
+
+    if(user){
+        return NextResponse.json({message: `User with the same ${firstKey}: '${userEmailOrPhone}' exist`, success: false}, {status: 201})
+    }
+      return NextResponse.json({message: "User not found", success: true}, {status: 200});
+}
 
 
